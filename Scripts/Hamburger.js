543caa75a80c4e82f6d5bd18dda1a322b7d5d8f4
Satyam Gupta <satyamgupta1495@gmail.com>
2021-06-21 18:54:33 +0530

    Responsive + Added Hamburger Menu

--- Scripts/Hamburger.js
+++ Scripts/Hamburger.js
@@ -1,8 +1,10 @@
 const togglebutton = document.getElementsByClassName("togglebutton")[0]
 const navlinks = document.getElementsByClassName("navbar-links")[0]
+const navlinks1 = document.getElementsByClassName("main-bar")[0]
 
 togglebutton.addEventListener("click", () => {
   navlinks.classList.toggle("active")
+  navlinks1.classList.toggle("active")
 })
 
 const navbar = document.querySelector(".section1")
