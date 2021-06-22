da51978ea63eb4ee3efc414cc3f5f9ad2f510a2b
Satyam Gupta <satyamgupta1495@gmail.com>
2021-06-22 15:41:55 +0530

    Fixed some Bugs

--- Scripts/Hamburger.js
+++ Scripts/Hamburger.js
@@ -1,10 +1,15 @@
 const togglebutton = document.getElementsByClassName("togglebutton")[0]
 const navlinks = document.getElementsByClassName("navbar-links")[0]
 const navlinks1 = document.getElementsByClassName("main-bar")[0]
+const navlinks2 = document.getElementsByClassName("button")[0]
+navlinks2.style.display = "flex"
+const navlinks3 = document.getElementsByClassName("enroll-now")[0]
+
 
 togglebutton.addEventListener("click", () => {
   navlinks.classList.toggle("active")
   navlinks1.classList.toggle("active")
+  navlinks3.classList.toggle("active")
 })
 
 const navbar = document.querySelector(".section1")
